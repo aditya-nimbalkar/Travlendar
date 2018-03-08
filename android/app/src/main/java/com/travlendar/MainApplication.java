172b998e7be4e0ba2bba9845423fc30f65769bb4
Alfred Gonsalves <alfredfgonsalves@gmail.com>
2018-03-07 17:12:18 -0700

    Task #192 - working on push notification device token

--- android/app/src/main/java/com/travlendar/MainApplication.java
+++ android/app/src/main/java/com/travlendar/MainApplication.java
@@ -3,7 +3,6 @@ package com.travlendar;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativePushNotificationPackage(),
             new RNFirebasePackage(),
             new RNFirebaseMessagingPackage()
       );
