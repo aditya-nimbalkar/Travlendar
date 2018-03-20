54cacb0d656ab8c5a4ccdc96f8dd84b6fcdcde4d
Alfred Gonsalves <alfredfgonsalves@gmail.com>
2018-03-19 20:36:10 -0700

    Task #192 - Setup the platform application and the platform endpoint manually Working on creating platform endpoint through the app

--- android/app/src/main/java/com/travlendar/MainApplication.java
+++ android/app/src/main/java/com/travlendar/MainApplication.java
@@ -3,6 +3,8 @@ package com.travlendar;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new RNFirebasePackage(),
             new RNFirebaseMessagingPackage()
       );
--- index.js
+++ index.js
@@ -5,9 +5,8 @@ import {
   View
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
 import Login from './src/components/Login/Login';
-// import PushNotification from 'react-native-push-notification';
+import PushNotification from 'react-native-push-notification';
 
 const NavigationApp = StackNavigator({
   Login: { screen: Login },
@@ -20,36 +19,57 @@ export default class TravlendarApp extends Component {
     );
   }
 
-  // componentDidMount() {
-  //     console.log('Did mount')
-  //     PushNotification.configure({
-  //       // (optional) Called when Token is generated (iOS and Android)
-  //       onRegister: function (token) {
-  //         console.log('TOKEN:', token);
-  //       },
-  //       // (required) Called when a remote or local notification is opened or received
-  //       onNotification: function (notification) {
-  //         console.log('NOTIFICATION:', notification);
-  //       },
-  //       // ANDROID ONLY: GCM Sender ID (optional — not required for local notifications, but is need to receive remote push notifications)
-  //       senderID: "383990736767",
-  //       // IOS ONLY (optional): default: all — Permissions to register.
-  //       permissions: {
-  //         alert: true,
-  //         badge: true,
-  //         sound: true
-  //       },
-  //       // Should the initial notification be popped automatically
-  //       // default: true
-  //       popInitialNotification: true,
-  //       /**
-  //       * (optional) default: true
-  //       * — Specified if permissions (ios) and token (android and ios) will requested or not,
-  //       * — if not, you must call PushNotificationsHandler.requestPermissions() later
-  //       */
-  //       requestPermissions: true,
-  //     });
-  //   }
+  componentDidMount() {
+      console.log('Did mount')
+      PushNotification.configure({
+        // (optional) Called when Token is generated (iOS and Android)
+        onRegister: function (token) {
+          console.log('TOKEN:', token);
+          var AWS = require('aws-sdk');
+          var sns = new AWS.SNS();
+          var device_token = token;
+          AWS.config.update({
+            accessKeyId: 'AKIAJMHM5F6BOYAZPSSA',
+            secretAccessKey: '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',
+            region: 'us-west-2'
+          });
+
+
+          sns.createPlatformEndpoint({
+            PlatformApplicationArn:  'arn:aws:sns:us-west-2:016911789346:app/GCM/Travlendar',
+            Token: device_token
+          }, function(err, data) {
+                if (err) {
+                  callback(null, JSON.stringify(err));
+                  console.log(err.stack);
+                  return;
+                }
+          });
+
+        },
+        // (required) Called when a remote or local notification is opened or received
+        onNotification: function (notification) {
+          console.log('NOTIFICATION:', notification);
+        },
+        // ANDROID ONLY: GCM Sender ID (optional — not required for local notifications, but is need to receive remote push notifications)
+        senderID: "383990736767",
+        // IOS ONLY (optional): default: all — Permissions to register.
+        permissions: {
+          alert: true,
+          badge: true,
+          sound: true
+        },
+        // Should the initial notification be popped automatically
+        // default: true
+        popInitialNotification: true,
+        /**
+        * (optional) default: true
+        * — Specified if permissions (ios) and token (android and ios) will requested or not,
+        * — if not, you must call PushNotificationsHandler.requestPermissions() later
+        */
+        requestPermissions: true,
+      });
+    }
 }
 
 AppRegistry.registerComponent('Travlendar', () => TravlendarApp);
