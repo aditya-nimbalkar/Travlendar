53a81241713a949b20533edd0aa8efef86469b4f
Alfred Gonsalves <alfredfgonsalves@gmail.com>
2018-04-01 18:25:57 -0700

    TG-223 #closed Creating endpoint works perfectly. Needs addition of username after login

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ export default class TravlendarApp extends Component {
           sns.createPlatformEndpoint({
             PlatformApplicationArn:  'arn:aws:sns:us-west-2:016911789346:app/GCM/Travlendar',
             Token: device_token,
-            CustomUserData: "Alfred"
+            CustomUserData: "User Email Here"
           }, function(err, data) {
                 if (err) {
                   // callback(null, JSON.stringify(err));
@@ -56,19 +56,7 @@ export default class TravlendarApp extends Component {
                   console.log("Successfully added device: ARN = " + data);
                   endpoint_arn = data.EndpointArn;
                   console.log("EndpointARN = " + endpoint_arn)
-                  sns.deleteEndpoint({
-                      EndpointArn: endpoint_arn,
-                  }, function(err, data) {
-                        if (err) {
-                          // callback(null, JSON.stringify(err));
-                          console.log(err.stack);
-                          return;
-                        }
-                        else {
-                          console.log("Successfully deleted device: " + data);
-                        }
-                  });
-
+                  
                 }
           });
 
