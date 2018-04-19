e87fced2393ad54c10836a733c8a6b984546a2a3
Aditya Nimbalkar <aditya.nimbalkar@asu.edu>
2018-04-18 17:54:09 -0700

    TG-284 Navigation Routing

--- App.js
+++ App.js
@@ -4,16 +4,23 @@ import { StackNavigator } from 'react-navigation';
 
 import LoginForm from './src/components/LoginForm';
 import RegistrationForm from './src/components/RegistrationForm';
+import ForgotPassword from './src/components/ForgotPassword';
 
-const NavigationApp = StackNavigator({
-  RegistrationForm: { screen: RegistrationForm }
-  // LoginForm: { screen: LoginForm }
-});
+const NavigationApp = StackNavigator(
+  {
+    RegistrationForm: { screen: RegistrationForm },
+    LoginForm: { screen: LoginForm },
+    ForgotPassword: { screen: ForgotPassword },
+  },
+  {
+    initialRouteName: 'LoginForm',
+  }
+);
 
 class App extends Component {
     render() {
       return (
-        <NavigationApp />
+        <NavigationApp/>
       );
     }
 }
